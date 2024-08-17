2228ed4b8493eb6b9fa9483a2b2a0dd1a03bb239
Alexandre <alexandregaia911@gmail.com>
2024-08-17 20:38:13 -0300

    deleting somethings

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,6 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store.js";
 
-if (process.env.NODE_ENV === "production") disableReactDevTools();
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
